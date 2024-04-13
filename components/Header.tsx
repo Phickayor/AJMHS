4fea0086ee26750e74bb093746f9c053a2bfd61b
Phickayor <jetawof@gmail.com>
2024-04-13 20:08:49 +0100

    Header Component

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="mx-auto w-10/12 py-5 flex justify-between">
+      <div className="flex gap-2">
+        <img src="/images/logo.png" alt="Amen Jet" className="w-16 h-16" />
+        <h1 className="self-center text-2xl">Amen Jet Medical Services</h1>
+      </div>
+      <ul className="flex gap-8 self-center">
+        <li>Home</li>
+        <li>Services</li>
+        <li>Gallery</li>
+        <li>Reviews</li>
+        <li>About us</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header
